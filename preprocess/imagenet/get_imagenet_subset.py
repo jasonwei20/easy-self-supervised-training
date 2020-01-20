4a351e447d1890b8fef97e7944eeb1b1e997922f
Jason Wei <jason.weng.wei@gmail.com>
2020-01-19 21:03:27 -0500

    add get imagenet subset code

--- preprocess/imagenet/get_imagenet_subset.py
+++ preprocess/imagenet/get_imagenet_subset.py
@@ -4,16 +4,22 @@ import random
 from multiprocessing import Pool
 random.seed(42)
 
-master_folder_path = Path("/home/ifsdata/vlg/ImageNet/CLS-LOC/train")
-output_master_folder_path = Path("/home/brenta/scratch/data/ImageNet_10_per_class/train")
-k_per_class = 10
+# master_folder_path = Path("/home/ifsdata/vlg/ImageNet/CLS-LOC/val")
+# output_master_folder_path = Path("/home/brenta/scratch/data/ImageNet_10_per_class/val")
+master_folder_path = Path("/home/brenta/scratch/data/imagenet_rotnet/val/")
+output_master_folder_path = Path("/home/brenta/scratch/data/temp_imagenet_rotnet/val")
+k_per_class = 1000
 
 def get_chunks(lst, n):
     """Yield n successive chunks from lst."""
     size = int(len(lst) / n)
     output_list = []
-    for i in range(0, len(lst), size):
-        output_list.append(lst[i:i + size])
+    for i in range(0, n):
+        sub_list = lst[i*size:i*size + size]
+        output_list.append(sub_list)
+    if len(lst) % n != 0:
+        for i in range((n-1)*size+1, len(lst)):
+            output_list[-1].append(lst[i])
     return output_list
 
 def confirm_output_folder(folder_path):
@@ -31,7 +37,7 @@ def copy_subfolder(subfolder):
     confirm_output_folder(output_subfolder)
 
     image_paths = get_image_paths(subfolder)
-    random.shuffle(image_paths)
+    # random.shuffle(image_paths)
     for image_path in image_paths[:k_per_class]:
         output_image_path = output_subfolder.joinpath(image_path.name)
         shutil.copyfile(image_path, output_image_path)
@@ -51,7 +57,7 @@ def copy_image_subset():
     # copy_subfolders(subfolders)
 
     #multi-thread
-    chunks = get_chunks(subfolders, 8)
+    chunks = get_chunks(subfolders, 4)
     p = Pool(8)
     p.map(copy_subfolders, chunks)
         
